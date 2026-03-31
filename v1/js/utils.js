c97e3b4493bb9865421620d7219f10b5c5066558
Vyacheslav Nuykin <nujkinvaceslav@gmail.com>
2026-03-31 06:42:21 +0300

    Update utils.js

--- v1/js/utils.js
+++ v1/js/utils.js
@@ -1,7 +1,7 @@
 const n1l = {
     formatDate: (date) => new Date(date).toLocaleString('ru-RU'),
 
-    fetchAPI: async (endpoint) => {
+    fetchN1lAPI: async (endpoint) => {
         const res = await fetch(`https://api.n1l.ru/v1/${endpoint}`);
         return res.json();
     }
